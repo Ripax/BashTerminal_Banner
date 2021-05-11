0aaa08de05500a05c1e05c1faf6a5ed0c2ede4d7
HTMLDigger <ripanbiswas007@gmail.com>
2021-05-11 21:12:21 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author : HTMLDigger
+# Date : Tuesday, 11 May 2021
+# for Banner examples
+# If not running interactively, don't do anything
+#*************************************************************************************
+
+# Bash color.
+
+sb='\033[0;33m' # Black - Regular
+g='\033[0;32m' # Green
+r='\033[0m'    # Text Reset
+
+#
+
+clear
+
+# Code start.
+
+echo -e "╔════════╗"
+echo -e "║${g}██${r}${sb}╗${r}  ${g}██${r}${sb}╗${r}║"
+echo -e "║${g}██${r}${sb}║${r}  ${g}██${r}${sb}║${r}║"
+echo -e "║${g}███████${r}${sb}║${r}${sb}${r}║"
+echo -e "║${g}██${r}${sb}╔══${r}${g}██${r}${sb}║${r}║"
+echo -e "║${g}██${r}${sb}║  ${r}${g}██${r}${sb}║${r}║"
+echo -e "║${sb}╚═╝  ╚═╝${r}║"
+echo -e "╚════════╝"
